6fadce336e2e479f002b3b993468905a7fe52178
Samuvel Tamilselvan Devasagayam <samuveltamilselvan.devasagayam@Fiserv.com>
2022-03-16 18:19:03 +0530

    Add project files.

---
+++ Samjeno.Docker.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Samjeno.Docker.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
